e4d3e1f0a4529a7f4215cc52462897a4bbc4d1b6
soohykeee <gur1028@skuniv.ac.kr>
2023-01-15 18:45:16 +0900

    Initial commit

---
+++ src/main/java/com/example/club/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.example.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig{
+
+}
