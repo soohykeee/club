69d4333a21d57090c4e8bc2e3f11a9621e08c4a4
soohykeee <gur1028@skuniv.ac.kr>
2023-01-22 17:01:12 +0900

    OAuth 2.0 구현을 위해 준비

---
+++ src/main/java/com/example/club/security/service/ClubOAuth2UserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.club.security.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        log.info("----------------------------------");
+        log.info("userRequest: " + userRequest);
+
+        return super.loadUser(userRequest);
+    }
+}
