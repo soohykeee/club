8071a36909e35d508f2d71a338213f1deda9f422
soohykeee <gur1028@skuniv.ac.kr>
2023-01-23 15:26:44 +0900

    OAuth 2.0 구현을 통해 구글 로그인 처리

--- src/main/java/com/example/club/security/service/ClubOAuth2UserDetailsService.java
+++ src/main/java/com/example/club/security/service/ClubOAuth2UserDetailsService.java
@@ -1,15 +1,30 @@
 package com.example.club.security.service;
 
+import com.example.club.entity.ClubMember;
+import com.example.club.entity.ClubMemberRole;
+import com.example.club.repository.ClubMemberRepository;
+import com.example.club.security.dto.ClubAuthMemberDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
+
+    private final ClubMemberRepository repository;
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -17,6 +32,61 @@ public class ClubOAuth2UserDetailsService extends DefaultOAuth2UserService {
         log.info("----------------------------------");
         log.info("userRequest: " + userRequest);
 
-        return super.loadUser(userRequest);
+        String clientName = userRequest.getClientRegistration().getClientName();
+
+        log.info("clientName : " + clientName);
+        log.info(userRequest.getAdditionalParameters());
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        log.info("=====================================");
+        oAuth2User.getAttributes().forEach((k, v) -> {
+            log.info(k + ":" + v);
+        });
+
+        String email = null;
+
+        if (clientName.equals("Google")) {
+            email = oAuth2User.getAttribute("email");
+        }
+
+        log.info("-------EMAIL : " + email);
+
+        ClubMember member = saveSocialMember(email);
+
+        ClubAuthMemberDTO clubAuthMember = new ClubAuthMemberDTO(
+                member.getEmail(),
+                member.getPassword(),
+                true,
+                member.getRoleSet().stream().map(
+                        role -> new SimpleGrantedAuthority("ROLE_" + role.name())
+                ).collect(Collectors.toList()),
+                oAuth2User.getAttributes()
+        );
+        clubAuthMember.setName(member.getName());
+
+        return clubAuthMember;
     }
+
+    private ClubMember saveSocialMember(String email) {
+        Optional<ClubMember> result = repository.findByEmail(email, true);
+
+        if (result.isPresent()) {
+            return result.get();
+        }
+
+        ClubMember clubMember = ClubMember.builder()
+                .email(email)
+                .name(email)
+                .password(passwordEncoder.encode("1111"))
+                .fromSocial(true)
+                .build();
+
+        clubMember.addMemberRole(ClubMemberRole.USER);
+
+        repository.save(clubMember);
+
+        return clubMember;
+    }
+
 }
