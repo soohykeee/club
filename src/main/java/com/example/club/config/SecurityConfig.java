b1f558473c7c768e723f251486ebc64cfd09e16a
soohykeee <gur1028@skuniv.ac.kr>
2023-01-27 21:32:36 +0900

    @PreAuthorize 추가

--- src/main/java/com/example/club/config/SecurityConfig.java
+++ src/main/java/com/example/club/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Log4j2
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -30,10 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/sample/all").permitAll()
-                .antMatchers("/sample/member").hasRole("USER")
-                .antMatchers("/sample/admin").hasRole("ADMIN");
+//        http.authorizeRequests()
+//                .antMatchers("/sample/all").permitAll()
+//                .antMatchers("/sample/member").hasRole("USER")
+//                .antMatchers("/sample/admin").hasRole("ADMIN");
 
         http.formLogin();
         http.csrf().disable();
--- src/main/java/com/example/club/controller/SampleController.java
+++ src/main/java/com/example/club/controller/SampleController.java
@@ -2,6 +2,7 @@ package com.example.club.controller;
 
 import com.example.club.security.dto.ClubAuthMemberDTO;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SampleController {
 
     @GetMapping("/all")
+    @PreAuthorize("permitAll()")
     public void exAll() {
         log.info("exAll................");
     }
@@ -26,6 +28,7 @@ public class SampleController {
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
     public void exAdmin() {
         log.info("exAdmin...............");
     }
