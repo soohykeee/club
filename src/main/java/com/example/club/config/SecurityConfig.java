66b357e0ae4588ab8409ee0b05102db8d41fdeb7
soohykeee <gur1028@skuniv.ac.kr>
2023-01-27 21:08:01 +0900

    Remember Me 추가 + 소셜 로그인 자동회원가입 시 수정 가능하도록 설정 예정

--- src/main/java/com/example/club/config/SecurityConfig.java
+++ src/main/java/com/example/club/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.club.config;
 
+import com.example.club.security.handler.ClubLoginSuccessHandler;
+import com.example.club.security.service.ClubUserDetailsService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,9 +15,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Log4j2
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private ClubUserDetailsService userDetailsService;
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ClubLoginSuccessHandler successHandler() {
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 
     @Override
@@ -26,8 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
         http.csrf().disable();
-
-        http.oauth2Login();
+        http.logout();
+        http.oauth2Login().successHandler(successHandler());
+        http.rememberMe().tokenValiditySeconds(60 * 60 * 7).userDetailsService(userDetailsService);
 
     }
 
